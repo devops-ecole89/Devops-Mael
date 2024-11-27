a9e29aed5732d13ba33d6fcc897cc3f9c498957b
maelogan <mael.dabard@gmail.com>
2024-11-27 09:50:24 +0100

    change test

--- flaskApp/flask_test.py
+++ flaskApp/flask_test.py
@@ -3,6 +3,6 @@ import myproject
 
 def testadd():
     assert myproject.add(1, 2) == 3
-    assert myproject.add(0, 0) == 0
-    assert myproject.add(-1, 1) == 0
+    assert myproject.add(1, 0) == 1
+    #assert myproject.add(-1, 1) == 0
 
