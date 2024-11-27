f947912a7ca335b460ccd58760cebfbd2d2ba772
maelogan <mael.dabard@gmail.com>
2024-11-27 11:32:16 +0100

    fix: echo in run.sh

--- flaskApp/run.sh
+++ flaskApp/run.sh
@@ -3,17 +3,14 @@
 # Exécuter les tests
 pytest
 TEST_RESULT=$?
-echo $TEST_RESULT
 
 # Vérifier si les tests ont réussi
 if [ $TEST_RESULT -eq 0 ]; then
-    #echo "Tests réussis, merge vers preprod"
-    echo $TEST_RESULT
+    echo "Tests réussis, merge vers preprod"
     #git checkout preprod
     #git merge dev
     #git push origin preprod
 else
-    #echo "Tests échoués, voir la sortie de pytest pour plus de détails"
-    echo $TEST_RESULT
+    echo "Tests échoués, voir la sortie de pytest pour plus de détails"
 
 fi
